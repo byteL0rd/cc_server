c1ca81b3265a62695f5e1ce22ddf801f2f0fec04
john5117 <abiodunogundijo@gmail.com>
2017-09-27 11:11:29 +0100

    htw students rewrite

---
+++ routes/views/format.ts
@@ -0,0 +1,45 @@
+export const students = `
+<h3 class="uk-text-bold uk-text-large"> HOW CAMPUS COUPONS WORKS </h3> <br><br>
+<h2 class="uk-text-lead"> What is a coupon </h2>
+<p> 	A coupon is ticket issued by a business owner (retail store or service provider), that can be redeemed for a financial discount when purchasing a product or obtaining a service.
+It entitles the holders to enjoy various offers on different products and services.
+Features of a typical coupon </p>
+
+<img class="uk-responsive-width"  src="/docs/cu_format.jpg">
+                            
+<p> NOTE: coupon code is the major feature on a coupon. Each coupon has a unique coupon code. The code is what qualifies the user to get the offer given by the business owner
+The couponer’s advantage  </p>
+                                     
+<img class="uk-responsive-width"  src="/docs/c_girl_adv.jpg">
+
+	Just in a phrase <h3 class="uk-text-bold">“YOU SAVE MORE”.</h3>
+	Your happy shopping experience is guaranteed, how? Because you enjoy viable discounts and other offers on your favourite product at your favourite store. 
+You spend less, you get more……ultimately, you save more!!!
+
+<h3 class="uk-text-bold"> USING THE WEBSITE (www.campuscoupons.ng) </h3>
+<p class="uk-text-justify">
+Opening of Account
+Each user (Student) opens an account on the website with his/her email, phone number and username.
+The email is used as login details with his/ her password.
+Upon log in, the user has access to his/her specific campus but can also go through offers from other schools. Offers are sorted based on the campus. Offers from various business owners are accessible by the user.
+Tokens (kupos)	
+The token which is our internal currency used to access the coupon codes of the offers on the website is popularly known as ‘KUPOS’
+Upon registration, each user is given 20 kupos. 
+Each user will be able to purchase Kupos directly on the website once the one they have has been exhausted. 
+ For a user without token, offers on the coupons cannot be accessed.
+Coupon Codes
+</p><br>
+<p>
+Upon using kupos to access a coupon, codes are revealed and these are downloadable. The codes generated are used to claim the offers on the coupons.
+The downloaded coupons saved on the mobile phone are taken to the store owners to claim the offer.
+Every coupon has expiry date. Every store is mandated to fulfil the offers on the coupons so far it has not expired.
+</p>
+<p class="uk-text-bold" style="font-style: italics">
+	
+
+For further enquiries
+contact us via email- campuscoupons@gmail.ng
+                 phone number- 08062784932, 08065268937, 07058843947, 08039535364, 07069360343
+</p>
+
+`
